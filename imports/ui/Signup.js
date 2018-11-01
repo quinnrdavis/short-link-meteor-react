83035461cbbe0bb50f914aba546f40d8dd40800b
Quinn R Davis <quinn.r.davis@gmail.com>
2018-11-01 09:44:23 -0500

    Update signup page language

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -61,7 +61,7 @@ class Signup extends React.Component {
                         <button className="button">Create Account</button>
                     </form>
 
-                    <Link to="/">Already have an account?</Link>
+                    <Link to="/">Have an account?</Link>
                 </div>
             </div>
         )
